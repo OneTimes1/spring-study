1eccb0e2d01006915861f22a0559383c8aeabb87
wangyaowei <1433076629@qq.com>
2023-11-05 22:39:09 +0800

    spring-ioc 获取bean的方式

---
+++ spring-ioc-xml/src/main/java/com/wang/spring6/UserTest.java
@@ -0,0 +1,23 @@
+package com.wang.spring6;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserTest {
+
+    private static Logger logger = LoggerFactory.getLogger(UserTest.class);
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        //根据id获取bean
+        User user = (User) context.getBean("user");
+        logger.info("根据id获取bean:"+user);
+        //根据类型获取bean
+        User user2 = context.getBean(User.class);
+        logger.info("根据类型获取bean:"+user2);
+        //根据类型和id获取bean
+        User user3 = context.getBean("user", User.class);
+        logger.info("根据类型和id获取bean:"+user3);
+    }
+}

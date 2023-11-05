4ceaf97cce062fd399f01c0c475024faa3154b30
wangyaowei <1433076629@qq.com>
2023-11-05 21:50:08 +0800

    spring-start 添加log4j2日志

--- sprig-start/src/main/java/com/wang/spring6/UserTest.java
+++ sprig-start/src/main/java/com/wang/spring6/UserTest.java
@@ -1,9 +1,13 @@
 package com.wang.spring6;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserTest {
+
+    private static Logger logger = LoggerFactory.getLogger(UserTest.class);
     public static void main(String[] args) {
         //加载spring的配置文件
         ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
@@ -12,5 +16,6 @@ public class UserTest {
         System.out.println(user);
         //调用user对象的相关方法
         user.add();
+        logger.debug("log4j日志测试");
     }
 }
